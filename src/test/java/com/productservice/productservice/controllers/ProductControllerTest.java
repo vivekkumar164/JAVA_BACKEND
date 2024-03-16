de3e2b51ca391f735e816bfee3b2498a533b50d4
Vivek kumar <vivekkumar031096@gmail.com>
2024-03-16 18:27:53 +0530

    mocking

--- src/test/java/com/productservice/productservice/controllers/ProductControllerTest.java
+++ src/test/java/com/productservice/productservice/controllers/ProductControllerTest.java
@@ -1,14 +1,19 @@
 package com.productservice.productservice.controllers;
 
+import com.productservice.productservice.dtos.GenericProductDto;
 import com.productservice.productservice.exceptions.ProductNotFoundException;
+import com.productservice.productservice.services.ProductService;
 import com.productservice.productservice.thirdPartyClients.fakeStoreClient.FakeStoreClient.FakeStoreClient;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class ProductControllerTest {
@@ -23,6 +28,9 @@ public class ProductControllerTest {
 //        ProductControllerTest(FakeStoreClient fakeStoreClient){
 //        this.fakeStoreClient = fakeStoreClient;
 //    }
+@MockBean
+    private ProductService productService;
+
     @Test
     @DisplayName("Testing 1 + 1 is 2")
     void testOnePlusOneIsTwoOrNot(){
@@ -36,5 +44,19 @@ public class ProductControllerTest {
     void testGetProductByIdNegativeTc() {
         assertThrows(ProductNotFoundException.class,() -> productController.getProductById(100L));
 
+    }
+@Test
+    void testGetProductByIdMocking() throws ProductNotFoundException {
+    GenericProductDto genericProductDto = new GenericProductDto();
+        when(productService.getProductById(any(Long.class))).thenReturn(genericProductDto);
+
+        //assertNull(productService.getProductById(100L));
+        assertEquals(genericProductDto,productService.getProductById(1000L));
+    }
+@Test
+    void testGetProductByIdMockingException() throws ProductNotFoundException {
+        when(productService.getProductById(1L))
+                .thenThrow(ProductNotFoundException.class);
+        assertThrows(ProductNotFoundException.class,() -> productController.getProductById(1L));
     }
 }
